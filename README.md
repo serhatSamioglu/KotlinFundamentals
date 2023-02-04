34a8cb9608d673d2c591ccf044468c648ce6a0b4
serhat samioglu <serhatsamioglu35@gmail.com>
2023-02-05 02:51:50 +0300

    InOutType

--- README.md
+++ README.md
@@ -184,10 +184,11 @@ fun getArea2(e: Shape2) =
 ```ruby
 /*
 When you declare a generic type with an in modifier,
-That means functions can take M as arguments but they can't return M:
+That means functions can take M as arguments but they can't return M
 
 When you declare a generic type with an out modifier,
-That means functions can return T but they can't take T as arguments:*/
+That means functions can return T but they can't take T as arguments
+*/
 interface Production<in M, out T> {
     fun produceIn(input: M)
     fun produceOut(): T
--- app/src/main/java/com/example/kotlinfundamentals/generics/InOutType.kt
+++ app/src/main/java/com/example/kotlinfundamentals/generics/InOutType.kt
@@ -2,10 +2,11 @@ package com.example.kotlinfundamentals.generics
 
 /*
 When you declare a generic type with an in modifier,
-That means functions can take M as arguments but they can't return M:
+That means functions can take M as arguments but they can't return M
 
 When you declare a generic type with an out modifier,
-That means functions can return T but they can't take T as arguments:*/
+That means functions can return T but they can't take T as arguments
+*/
 interface Production<in M, out T> {
     fun produceIn(input: M)
     fun produceOut(): T
