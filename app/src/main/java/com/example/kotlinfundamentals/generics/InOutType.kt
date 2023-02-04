fa7bbd543896879c377765ebe405f62282c0084d
serhat samioglu <serhatsamioglu35@gmail.com>
2023-02-05 01:20:37 +0300

    InOutType

---
+++ app/src/main/java/com/example/kotlinfundamentals/generics/InOutType.kt
@@ -0,0 +1,28 @@
+package com.example.kotlinfundamentals.generics
+
+/*
+When you declare a generic type with an in modifier,
+That means functions can take M as arguments but they can't return M:
+
+When you declare a generic type with an out modifier,
+That means functions can return T but they can't take T as arguments:*/
+interface Production<in M, out T> {
+    fun produceIn(input: M)
+    fun produceOut(): T
+}
+
+class Product: Production<Int, String> {
+    override fun produceIn(input: Int) {
+        println(input.toString())
+    }
+
+    override fun produceOut(): String {
+        return "produceOut"
+    }
+}
+
+fun main() {
+    val product = Product()
+    product.produceIn(0)
+    println(product.produceOut())
+}
