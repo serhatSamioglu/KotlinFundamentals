0bbde5901283bfc6e79f705d903e7c078fa4b146
serhat samioglu <serhatsamioglu35@gmail.com>
2023-02-21 22:01:42 +0300

    Coroutines

---
+++ app/src/main/java/com/example/kotlinfundamentals/homepage/HomeActivity.kt
@@ -0,0 +1,26 @@
+package com.example.kotlinfundamentals.homepage
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlinfundamentals.coroutines.CoroutinesActivity
+import com.example.kotlinfundamentals.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        handleClickListeners()
+    }
+
+    private fun handleClickListeners() {
+        binding.buttonCoroutines.setOnClickListener {
+            startActivity(Intent(this, CoroutinesActivity::class.java))
+        }
+    }
+}
